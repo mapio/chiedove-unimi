d457bee06b1dde059830369d69bfdf3fcf01bc6f
Massimo Santini <santini@di.unimi.it>
2013-11-25 14:34:22 +0100

    First release

---
+++ Makefile
@@ -0,0 +1,11 @@
+PYTHON=/usr/bin/env python
+
+chiedove: chiedove-unimi/__main__.py
+	zip --quiet --junk-paths chiedove-unimi chiedove-unimi/__main__.py
+	echo '#!$(PYTHON)' > chiedove
+	cat chiedove-unimi.zip >> chiedove
+	rm chiedove-unimi.zip
+	chmod a+x chiedove
+
+clean:
+	@rm chiedove chiedove.zip
---
+++ chiedove-unimi/__main__.py
@@ -0,0 +1,14 @@
+from xml.etree import ElementTree
+from urllib import urlopen
+from sys import argv
+
+URL = 'http://www.unimi.it/chiedove/listaPersoneXML.jsp?surname={surname}&name={name}&tipoOrganico=0'
+
+surname = argv[ 1 ] if len( argv ) >= 2 else ''
+name = argv[ 2 ] if len( argv ) == 3 else ''
+
+data = urlopen( URL.format( name = name, surname = surname ) )
+
+root = ElementTree.parse( data ).getroot()
+rows = next( root.iter( '{http://www.w3.org/1999/xhtml}tbody' ) ).iter( '{http://www.w3.org/1999/xhtml}tr' )
+print '\n'.join( '\t'.join( filter( None, [ _.text for _ in r.iter() ] ) ) for r in rows )
