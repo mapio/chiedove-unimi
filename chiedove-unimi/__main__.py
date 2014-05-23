b2d16b93f245689f286fdcadcab2733206f0fd74
Massimo Santini <santini@di.unimi.it>
2014-05-23 14:16:26 +0200

    Fixed wrt an error introduced in the XML response from Unimi website

--- chiedove-unimi/__main__.py
+++ chiedove-unimi/__main__.py
@@ -1,5 +1,6 @@
 from xml.etree import ElementTree
 from urllib import urlopen
+from re import sub, DOTALL
 from sys import argv
 
 URL = 'http://www.unimi.it/chiedove/listaPersoneXML.jsp?surname={surname}&name={name}&tipoOrganico=0'
@@ -7,11 +8,11 @@ URL = 'http://www.unimi.it/chiedove/listaPersoneXML.jsp?surname={surname}&name={
 surname = argv[ 1 ] if len( argv ) >= 2 else ''
 name = argv[ 2 ] if len( argv ) == 3 else ''
 
-data = urlopen( URL.format( name = name, surname = surname ) )
-
-root = ElementTree.parse( data ).getroot()
 try:
+	data = urlopen( URL.format( name = name, surname = surname ) )
+	data = sub( r'<script.*?</script>', '', data.read(), flags = DOTALL )
+	root = ElementTree.fromstring( data )
 	rows = next( root.iter( '{http://www.w3.org/1999/xhtml}tbody' ) ).iter( '{http://www.w3.org/1999/xhtml}tr' )
 	print '\n'.join( '\t'.join( filter( None, [ _.text for _ in r.iter() ] ) ) for r in rows )
-except StopIteration:
+except:
 	pass
