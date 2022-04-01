4e75cc510958b3494c0cddad0d21dd665e5dc44d
Massimo Santini <santini@di.unimi.it>
2022-04-01 09:31:22 +0200

    Added user agent header to defeat extra UniMI security

--- chiedove-unimi/__main__.py
+++ chiedove-unimi/__main__.py
@@ -1,29 +1,45 @@
 from json import loads
 from re import finditer, DOTALL
 from sys import argv
-from urllib.request import urlopen
+from urllib import request, parse
 
-SEARCH_URL = 'https://www.unimi.it/it/views/ajax?_wrapper_format=drupal_ajax' 
-data = 'cognome={}&nome=&ur_tipo_ruolo_target_id=All&view_name=chi_e_dove&view_display_id=block_1&view_args=&view_path=%2Fit%2Fchi-e-dove&view_base_path=&view_dom_id=0&pager_element=0&_drupal_ajax=1'
-
+SEARCH_URL = 'https://www.unimi.it/it/views/ajax'
 DETAIL_URL = 'https://www.unimi.it/it/ugov/person/'
-    
+
 name = argv[1]
 
-try:
-    people = []
-    for item in loads(urlopen(SEARCH_URL, data.format(name).encode('utf-8')).read().decode('utf-8')):
-        if item['command'] == 'insert':
-            for match in finditer(r'<a href="/it/ugov/person/([^"]+)"', item['data']):
-                people.append(match.group(1))
+data = parse.urlencode({
+    'cognome': name,
+    'nome': '',
+    'ur_tipo_ruolo_target_id': 'All',
+    'view_name': 'chi_e_dove',
+    'view_display_id': 'block_1',
+    'view_args': '',
+    'view_path': '/node/7663',
+    'view_base_path': '',
+    'view_dom_id': 'ece5376832f8559006242f5655147c55cd303248b9c1f7cc4627f7021f4537d4',
+    'pager_element': '1',
+    '_drupal_ajax': '1',
+}).encode('utf-8')
 
-    DETAIL_URL = 'https://www.unimi.it/it/ugov/person/'
-    for person in people:
-        data = urlopen(DETAIL_URL + person).read().decode('utf-8')
-        info = [match.group(1).strip() for match in finditer(r'<title>([^<]+)\|.*</title>', data)]
-        if not name in info[0].lower(): continue
-        info.extend(match.group(1).strip() for match in finditer(r'<a href="mailto:([^"]+)"', data))
-        info.extend(match.group(1).strip() for match in finditer(r'<div\s+class="pad-icon">([^<]+)</div>', data))
-        print('\t'.join(info))
-except:
-    pass
+response = request.urlopen(
+    request.Request(SEARCH_URL, data = data, headers = {'User-Agent': 'Mozilla/5.0'})
+).read().decode('utf-8')
+
+#try:
+people = []
+for item in loads(response):
+    if item['command'] == 'insert':
+        for match in finditer(r'<a href="/it/ugov/person/([^"]+)"', item['data']):
+            people.append(match.group(1))
+
+DETAIL_URL = 'https://www.unimi.it/it/ugov/person/'
+for person in people:
+    data = request.urlopen(request.Request(DETAIL_URL + person, headers = {'User-Agent': 'Mozilla/5.0'})).read().decode('utf-8')
+    info = [match.group(1).strip() for match in finditer(r'<title>([^<]+)\|.*</title>', data)]
+    if not name in info[0].lower(): continue
+    info.extend(match.group(1).strip() for match in finditer(r'<a href="mailto:([^"]+)"', data))
+    info.extend(match.group(1).strip() for match in finditer(r'<div\s+class="pad-icon">([^<]+)</div>', data))
+    print('\t'.join(info))
+#except:
+#   pass
